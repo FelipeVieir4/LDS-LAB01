e9d1f62e742b455c0866cb073d7843a442f7d1a4
Renato-Paganini-Thurler <paganini.xxi@gmail.com>
2023-08-27 20:58:04 -0300

    "métodos do Professor criado"

--- Implementação/Professor.java
+++ Implementação/Professor.java
@@ -1,3 +1,20 @@
+import java.util.List;
+
 public class Professor {
+    private String login;
+    private String senha;
+
+    public Professor() {
+
+    }
+
+    public void salvar(String login, String senha) {
+        this.login = login;
+        this.senha = senha;
+    }
+
+    public List<Aluno> consultarAlunosPorDisciplina(Disciplina disciplina) {
+        return disciplina.getAlunos();
+    }
 
 }
