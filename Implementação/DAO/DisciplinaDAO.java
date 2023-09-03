e1efc2f615a97e0136f8074b43bcf8fe49028c8e
Renato-Paganini-Thurler <paganini.xxi@gmail.com>
2023-09-02 22:34:53 -0300

    "Disciplina DAO"

--- Implementação/DAO/DisciplinaDAO.java
+++ Implementação/DAO/DisciplinaDAO.java
@@ -2,6 +2,42 @@ package DAO;
 
 import ModelController.Disciplina;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Classe que representa um DAO (Data Access Object) para a classe Disciplina.
+ */
 public class DisciplinaDAO {
 
+    private static final String FILENAME = "disciplinas.csv";
+
+    /**
+     * Salva uma lista de disciplinas em um arquivo CSV.
+     *
+     * @param disciplinas A lista de disciplinas a ser salva.
+     * @throws IOException Se ocorrer um erro ao salvar o arquivo.
+     */
+    public void salvar(List<Disciplina> disciplinas) throws IOException {
+        FileWriter writer = new FileWriter(FILENAME);
+
+        // Escreve o cabeçalho do arquivo CSV
+        writer.append("ProximoID,Nome,Credito,Tipo\n");
+
+        // Escreve os dados das disciplinas no arquivo CSV
+        for (Disciplina disciplina : disciplinas) {
+            writer.append(String.valueOf(disciplina.getProximoID()));
+            writer.append(',');
+            writer.append(disciplina.getNome());
+            writer.append(',');
+            writer.append(String.valueOf(disciplina.getCredito()));
+            writer.append(',');
+            writer.append(disciplina.getTipo().toString());
+            writer.append('\n');
+        }
+
+        writer.flush();
+        writer.close();
+    }
 }
