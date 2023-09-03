47357ccbc9e916b3509c2e5cce8302a92baef011
Renato-Paganini-Thurler <paganini.xxi@gmail.com>
2023-09-02 22:17:02 -0300

    "Curso atributes for DAO"

--- Implementação/ModelController/Curso.java
+++ Implementação/ModelController/Curso.java
@@ -1,5 +1,23 @@
 package ModelController;
 
+import java.util.List;
+
 public class Curso {
-    
+    private int id;
+    private String nome;
+    private List<Disciplina> disciplinas;
+
+    // Métodos de acesso
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public List<Disciplina> getDisciplinas() {
+        return disciplinas;
+    }
+
 }
